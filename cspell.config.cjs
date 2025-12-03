66f6ed2504be2633d30ee52eb1470788772bbd0d
ahoxa <yujinyuto1223@gmail.com>
2025-12-03 12:36:10 +0900

    feat: install markdownlint and cspell for linter

---
+++ cspell.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('cspell').CSpellUserSettings} */
+module.exports = {
+  version: "0.2",
+  language: "en,ja",
+  files: ["articles/**/*.md"],
+  ignorePaths: ["node_modules", "dist"],
+  words: ["Zenn", "textlint", "zenncli", "Ahoxa", "reacthooks", "hrbrain"],
+};
